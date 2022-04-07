19c233d865dc464c20f93bcf4e76a9eee971c5be
Fernando Rojo <frojo@sas.upenn.edu>
2022-04-06 22:20:44 -0400

    ext

--- script.js
+++ script.js
@@ -1,35 +1,13 @@
-const storiesDiv = document.querySelector('#stories');
-const storyNode = (story) => {
-  var template = document.createElement('template');
-  template.innerHTML = story;
-  return template.content.childNodes[0];
+var src = images[Math.floor(Math.random() * images.length)];
+
+var img = document.createElement("img");
+img.src = src;
+document.getElementById("img").appendChild(img);
+
+var link = document.querySelector("link[rel~='icon']");
+if (!link) {
+  link = document.createElement("link");
+  link.rel = "icon";
+  document.getElementsByTagName("head")[0].appendChild(link);
 }
-const addStories = (stories) => {
-  for (let index in stories) {
-    const story = stories[index];
-    const html = `<div class="story">
-      <a href="${story.url}">${story.text}</a>
-    </div>`;
-    storiesDiv.appendChild(storyNode(html));
-  }
-}
-if (localStorage.lastFetch && localStorage.stories && (new Date() - localStorage.lastFetch) < (1000*60*60)) {
-  addStories(JSON.parse(localStorage.stories));
-} else {
-  if (localStorage.stories) {
-    addStories(JSON.parse(localStorage.stories));
-  }
-  fetch('https://api.hackernoon.com/featured-stories',{
-      method: 'GET',
-      mode: 'cors',
-      credentials: 'include'
-    })
-    .then(response => response.json())
-    .then(data => {
-      if (!localStorage.stories) {
-        addStories(data);
-      }
-      localStorage.setItem("stories", JSON.stringify(data));
-      localStorage.setItem("lastFetch", new Date()-1);
-    });
-}
+link.href = src;
